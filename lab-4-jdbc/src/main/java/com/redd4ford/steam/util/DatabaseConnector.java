3ca4612a5be8684c5c38d7aee16b7632dc43131c
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-11-15 14:18:57 +0200

    Added util database connector

---
+++ lab-4-jdbc/src/main/java/com/redd4ford/steam/util/DatabaseConnector.java
@@ -0,0 +1,38 @@
+package com.redd4ford.steam.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnector {
+
+  private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+  private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/steam";
+  private static final String USERNAME = "root";
+  private static final String PASSWORD = System.getenv("PASSWORD");
+
+  private static Connection DATABASE_CONNECTION;
+
+  public DatabaseConnector() {
+  }
+
+  public static Connection getConnection() {
+    try {
+      Class.forName(JDBC_DRIVER);
+      if (DATABASE_CONNECTION == null || DATABASE_CONNECTION.isClosed()) {
+        try {
+          DATABASE_CONNECTION = DriverManager.getConnection(
+              DATABASE_URL,
+              USERNAME,
+              PASSWORD
+          );
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return DATABASE_CONNECTION;
+  }
+
+}
