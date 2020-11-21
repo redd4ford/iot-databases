5cfe75b4fbcf7817d3ea289f3d79e4bc5be6b1fa
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-11-21 02:04:57 +0200

    Added Application to handle the session

---
+++ lab-5-hibernate/src/main/java/com/redd4ford/steam/Application.java
@@ -0,0 +1,18 @@
+package com.redd4ford.steam;
+
+import com.redd4ford.steam.util.HibernateUtil;
+import com.redd4ford.steam.view.View;
+import org.hibernate.Session;
+
+public class Application {
+
+  public static void main(String[] args) {
+    try (Session SESSION = HibernateUtil.getSession()) {
+      System.out.println("Test session established. ");
+      new View(SESSION).show();
+    } catch (Throwable ex) {
+      System.err.println("Test session failed.");
+      ex.printStackTrace();
+    }
+  }
+}
